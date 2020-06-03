063866cc23a75511601758c26b7503960cb5743a
Flynn <crflynn@users.noreply.github.com>
2020-06-03 01:33:21 -0400

    build setup (#33)
    * build setup
    
    * fixes

---
+++ buildpre.py
@@ -0,0 +1,18 @@
+import os
+import shutil
+
+top = os.path.dirname(os.path.abspath(__file__))
+
+
+def copy_ranger_source():
+    """Copy the ranger cpp source, following symlinks."""
+    src = os.path.join(top, "ranger", "cpp_version")
+    dst = os.path.join(top, "skranger", "ensemble", "ranger")
+    try:
+        shutil.rmtree(dst)
+    except FileNotFoundError:
+        pass
+    shutil.copytree(src, dst, symlinks=False)
+
+
+copy_ranger_source()
