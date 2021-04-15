683f9487c3aa6fafc7403eb7aa8b52e3ca5e805d
Kevin MacDonald <kevinmacdonald4@gmail.com>
2021-04-15 01:56:34 +0000

    Fix pvalue test issues by adding random columns to Iris

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,13 @@
 import math
 
 import pytest
+import numpy as np
 from sklearn.datasets import load_boston
-from sklearn.datasets import load_digits
 from sklearn.datasets import load_iris
 from sksurv.datasets import load_veterans_lung_cancer
 
 _boston_X, _boston_y = load_boston(True)
 _iris_X, _iris_y = load_iris(True)
-_digits_X, _digits_y = load_digits(10, True)
 _lung_X, _lung_y = load_veterans_lung_cancer()
 
 
@@ -27,19 +26,27 @@ def iris_X():
     return _iris_X
 
 
+@pytest.fixture(params=['std','rand','rand_const'])
+def mod(request):
+    return request.param
+
+
+@pytest.fixture
+def iris_X_mod(mod):
+    if mod == 'std':
+        return _iris_X
+    elif mod == 'rand':
+        np.random.seed(42)
+        return np.concatenate((_iris_X, np.random.uniform(size=(_iris_X.shape[0],1))),1)
+    elif mod == 'rand_const':
+        np.random.seed(42)
+        return np.concatenate((_iris_X, np.random.uniform(size=(_iris_X.shape[0],3)), np.zeros(shape=(_iris_X.shape[0],3))),1)
+        
+
+
 @pytest.fixture
 def iris_y():
     return _iris_y
-
-
-@pytest.fixture
-def digits_X():
-    return _digits_X
-
-
-@pytest.fixture
-def digits_y():
-    return _digits_y
 
 
 @pytest.fixture
