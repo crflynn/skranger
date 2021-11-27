69287bb8b293b612114cab11a48d98ba6b6d8123
flynn <crf204@gmail.com>
2021-11-26 21:18:41 -0500

    fix value for classifier

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -1,5 +1,6 @@
 import pytest
 
+from skranger.ensemble import RangerForestClassifier
 from skranger.ensemble import RangerForestRegressor
 
 
@@ -24,25 +25,51 @@ def test_plot():
 
 # FIXME not working yet
 @pytest.mark.skip()
-def test_shap(boston_X, boston_y):
+def test_shap_regressor(boston_X, boston_y):
     from shap import TreeExplainer
 
     forest = RangerForestRegressor()
     forest.fit(boston_X, boston_y)
 
-    explainer = TreeExplainer(model=forest, data=boston_X)
-    shap_values = explainer.shap_values(boston_X, check_additivity=True)
+    explainer = TreeExplainer(model=forest)
+    shap_values = explainer.shap_values(boston_X)
+    print(shap_values)
+
+
+# FIXME not working yet
+@pytest.mark.skip()
+def test_shap_classifier(iris_X, iris_y):
+    from shap import TreeExplainer
+
+    forest = RangerForestClassifier(enable_tree_details=True)
+    forest.fit(iris_X, iris_y)
+
+    explainer = TreeExplainer(model=forest)
+    shap_values = explainer.shap_values(iris_X)
     print(shap_values)
 
 
 @pytest.mark.skip()
-def test_shap_sklearn(boston_X, boston_y):
+def test_shap_sklearn_regressor(boston_X, boston_y):
     from shap import TreeExplainer
     from sklearn.ensemble import RandomForestRegressor
 
     forest = RandomForestRegressor()
     forest.fit(boston_X, boston_y)
 
-    explainer = TreeExplainer(model=forest, data=boston_X)
-    shap_values = explainer.shap_values(boston_X, check_additivity=True)
+    explainer = TreeExplainer(model=forest)
+    shap_values = explainer.shap_values(boston_X)
+    print(shap_values)
+
+
+@pytest.mark.skip()
+def test_shap_sklearn_classifier(iris_X, iris_y):
+    from shap import TreeExplainer
+    from sklearn.ensemble import RandomForestClassifier
+
+    forest = RandomForestClassifier()
+    forest.fit(iris_X, iris_y)
+
+    explainer = TreeExplainer(model=forest)
+    shap_values = explainer.shap_values(iris_X)
     print(shap_values)
