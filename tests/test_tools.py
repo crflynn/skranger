f17fdce88a44f66f2dc9f799f7ee9a3f124be51f
flynn <crf204@gmail.com>
2021-11-29 02:38:45 -0500

    fix

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -1,8 +1,8 @@
+import os
 import sys
 from contextlib import contextmanager
 
 import pytest
-from shap import TreeExplainer
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.tree._tree import Tree as SKTree
@@ -59,8 +59,12 @@ def test_plot():
     )
 
 
-@pytest.mark.skipif(sys.version_info > (3, 9), reason="requires < 3.9")
+@pytest.mark.skipif(
+    sys.version_info > (3, 9) and os.getenv("CI"), reason="requires < 3.9"
+)
 def test_shap_regressor(boston_X, boston_y):
+    from shap import TreeExplainer
+
     forest = RangerForestRegressor(enable_tree_details=True)
     forest.fit(boston_X, boston_y)
 
@@ -70,8 +74,12 @@ def test_shap_regressor(boston_X, boston_y):
     print(shap_values)
 
 
-@pytest.mark.skipif(sys.version_info > (3, 9), reason="requires < 3.9")
+@pytest.mark.skipif(
+    sys.version_info > (3, 9) and os.getenv("CI"), reason="requires < 3.9"
+)
 def test_shap_classifier(iris_X, iris_y):
+    from shap import TreeExplainer
+
     forest = RangerForestClassifier(enable_tree_details=True)
     forest.fit(iris_X, iris_y)
 
@@ -83,6 +91,8 @@ def test_shap_classifier(iris_X, iris_y):
 
 @pytest.mark.skip()
 def test_shap_sklearn_regressor(boston_X, boston_y):
+    from shap import TreeExplainer
+
     forest = RandomForestRegressor()
     forest.fit(boston_X, boston_y)
 
@@ -93,6 +103,8 @@ def test_shap_sklearn_regressor(boston_X, boston_y):
 
 @pytest.mark.skip()
 def test_shap_sklearn_classifier(iris_X, iris_y):
+    from shap import TreeExplainer
+
     forest = RandomForestClassifier()
     forest.fit(iris_X, iris_y)
 
